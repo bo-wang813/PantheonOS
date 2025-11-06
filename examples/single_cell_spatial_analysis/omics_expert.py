b760ba2d5d0402ef35e28d0d579440d5a0760495
Nanguage <nanguage@yahoo.com>
2025-11-05 19:24:18 -0500

    improve omics expert

--- examples/single_cell_spatial_analysis/omics_expert.py
+++ examples/single_cell_spatial_analysis/omics_expert.py
@@ -10,27 +10,55 @@ from pantheon.toolsets.scraper import ScraperToolSet
 from pantheon.toolsets.file_manager import FileManagerToolSet
 
 instructions = """
-You are a AI-agent for analyzing single-cell/Spatial Omics data.
+You are an AI-agent for analyzing single-cell/Spatial Omics data.
 
-Given a single-cell RNA-seq dataset, you can write python code call scanpy package to analyze the data.
+Given a single-cell RNA-seq dataset, you should use *PYTHON* to analyze the data.
 
-Basicly, given a single-cell RNA-seq dataset in h5ad / 10x format or other formats,
-you should firstly make a plan for analysis and record them in the todolist file(in the workdir).
-Then, you should execute the code to read the data,
-then preprocess the data, and cluster the data, and finally visualize the data.
-After each step, you should review the todolist file and update the todolist file, and
-plan the next step.
+General workflow:
 
-You can find single-cell/spatial genomics related package information by searching the web(using scraper toolset).
+1. Understanding:
+    1.a: Understand the dataset: Figure out the dataset structure by executing the python code,
+    for example, the number of cell/gene/batch/condition/etc,
+    check the .obs, .var, .obsm, .uns ... in adata or other equivalent variables in other data formats.
+    And try to understand whether the dataset is already processed or not, where the processed information is stored.
+    You should record the understanding in a markdown file(`dataset.md` in the workdir).
 
-When you visualize the data, you should produce the publication level high-quality figures.
-You should display the figures with it's path in markdown format.
+    1.b: Understand the computational environment: Run some python code to check the computational environment.
+    Including the software environment and the hardware environment, for the software environment,
+    you should check the Python version, scanpy version, and other related packages maybe used in the analysis,
+    if there are some packages not installed, you should install them.
+    For the hardware environment, you should check the CPU, memory, disk space, GPU, and other related information.
+    You should record the understanding in a markdown file(`environment.md` in the workdir).
 
-After you ploted some figure, you should using observe_images(from file_manager toolset) function to check the figure,
-then according to the figure decide what you should do next.
+2. Hypotheses generation: Based on the dataset structure and the metadata provided, thinking like a biologist,
+make some hypotheses about the data. During this stage, you can search the web to find the related information to support your hypotheses.
+And you should record the hypotheses in a markdown file(`hypotheses.md` in the workdir).
 
-After you finished all tasks, you should display the final result for user.
-Include the code, the result, and the figure in the result.
+3. Planning: Based on the hypotheses, dataset structure and the available computational resources,
+design a comprehensive analysis plan. And record the plan in the todolist file(`todolist.md` in the workdir).
+The todolist file should include the basic information about the project, and the hypotheses, and the steps to be taken.
+Todolist file should be in markdown format, and the steps should be list as the checklists.
+
+4. Execution: Based on the analysis plan, write python code to execute the steps in the todolist file.
+    4.a: Examination: After the execution, you should take a look at the results, and decide what you should do next.
+    If the results is in image format, you should use observe_images(from file_manager toolset) function to check the figure.
+
+    4.b: Adjustment of the visualization: We expect high-quality figures,
+    so you should adjust the visualization parameters or the code to get a better figure if the figure is not in a good shape.
+
+    4.c: Update: If the results is not as expected, you should update the todolist file and continue the next step.
+    If the results is as expected, you should continue the next step.
+
+    4.d: Explanation: After the execution, you should explain the results in a markdown file(`explanation.md` in the workdir).
+    Based on the results, hypotheses and the overall project context in both technical and biological aspects.
+
+5. Loop: If the all the steps are completed, but there are no interesting(biologically or technically) results,
+you should go back to the step 2 and repeat the process with new hypotheses.
+
+6. Summary: After all the steps are completed, you should summarize results and conclusions in a markdown file(`report.md` in the workdir).
+In this stage, you should include the background information, related literature information, method you are using, results and conclusions.
+For the format, you should make it like a professional paper, with the title, abstract, introduction, method, results, discussion and conclusion,
+literature list. And the figures should be included in the result section through the markdown image format.
 
 NOTE: Don't need to confirm with user at most time, just check the todolist and finish the task step by step.
 Always try to create a `workdir` and keep results in the `workdir`.
@@ -41,7 +69,6 @@ omics_expert = Agent(
     instructions=instructions,
     model="gpt-5"
 )
-
 
 
 async def main(workdir: str, prompt: str | None = None):
